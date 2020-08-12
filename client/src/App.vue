32485cd148d0d28817a96bde57df1967274fc619
wipadanan <nanwipada2542@gmail.com>
2020-08-12 23:56:30 +0700

    folder perpared

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <back-header />
     <router-view/>
   </div>
 </template>
@@ -16,6 +17,5 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -7,9 +7,22 @@ import UserCreate from '@/components/Users/CreateUser'
 import UserEdit from '@/components/Users/EditUser'
 import UserShow from '@/components/Users/ShowUser'
 
+// Authen
+import Login from '@/components/Login'
+
+// Comments
+import CommentIndex from '@/components/Comments/Index'
+
+// Blogs
+import BlogIndex from '@/components/Blogs/Index'
+import BlogCreate from '@/components/Blogs/CreateBlog'
+import BlogEdit from '@/components/Blogs/EditBlog'
+import BlogShow from '@/components/Blogs/ShowBlog'
+
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/Users',
@@ -30,6 +43,39 @@ export default new Router({
       path: '/user/:userId',
       name: 'user',
       component: UserShow
+    },
+    //authen
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    // blogs
+    {
+    path: '/blogs',
+    name: 'blogs',
+    component: BlogIndex
+    },
+    {
+    path: '/blog/create',
+    name: 'blogs-edit',
+    component: BlogCreate
+    },
+    {
+    path: '/blog/edit/:blogId',
+    name: 'blog-edit',
+    component: BlogEdit
+    },
+    {
+    path: '/blog/:blogId',
+    name: 'blog',
+    component: BlogShow
+    },
+    // comments
+    {
+    path: '/comments',
+    name: 'comments',
+    component: CommentIndex
     },
   ]
 })
